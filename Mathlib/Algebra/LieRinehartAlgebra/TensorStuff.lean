efee868852fcf413a76625c078b59e9db27ba438
leo <leonid.ryvkin@rub.de>
2026-03-13 18:17:41 +0100

    tensorproduct helper file completed

--- Mathlib/Algebra/LieRinehartAlgebra/TensorStuff.lean
+++ Mathlib/Algebra/LieRinehartAlgebra/TensorStuff.lean
@@ -12,9 +12,6 @@ open TensorProduct
 abbrev span_of_smul_tmul :=
   (Submodule.span A {x | ∃ (a : A) (m: M) (n : N), (a • m) ⊗ₜ[R] n - m ⊗ₜ[R] (a • n) = x})
 
-
-#check Submodule.mkQ
-
 def map_to_quot : M →ₗ[A] N →ₗ[A] ((M⊗[R] N) ⧸ (span_of_smul_tmul R A M N)) where
   toFun m := {
     toFun n := (span_of_smul_tmul R A M N).mkQ (m⊗ₜn)
@@ -37,18 +34,69 @@ def map_to_quot : M →ₗ[A] N →ₗ[A] ((M⊗[R] N) ⧸ (span_of_smul_tmul R 
     rw [←LinearMap.map_smul, ←sub_eq_zero, ← LinearMap.map_sub,smul_tmul']
     simp
 
+omit [IsScalarTower R A N] in
+@[simp]
+lemma map_to_quot_apply (m : M) (n : N) : map_to_quot R A M N m n = ⟦m⊗ₜn⟧ := by rfl
+
+
+
 abbrev tens_to_quot := TensorProduct.lift (map_to_quot R A M N)
 
--- now I should show that the span is in the kernel of  (mapOfCompatibleSMul' A R M N), and
--- use this to obtain a map quot to tens and then put them together to have an iso between
--- the tensor product an the quotient. This then can be used to prove the lemma below
+omit [IsScalarTower R A N] in
+@[simp]
+lemma tens_to_quot_apply (m : M) (n : N) : tens_to_quot R A M N (m⊗ₜn) = ⟦m⊗ₜn⟧ := by rfl
 
 
+lemma compose_eq_mkQ :(tens_to_quot R A M N) ∘ₗ (mapOfCompatibleSMul' A R M N)
+    = (span_of_smul_tmul R A M N).mkQ := by
+  ext m n
+  simp [mapOfCompatibleSMul', Submodule.Quotient.mk''_eq_mk]
 
 
+lemma span_in_ker : (span_of_smul_tmul R A M N) ≤ (mapOfCompatibleSMul' A R M N).ker := by
+  rw [LinearMap.le_ker_iff_comp_subtype_eq_zero]
+  let S := {x | ∃ (a : A) (m: M) (n : N), (a • m) ⊗ₜ[R] n - m ⊗ₜ[R] (a • n) = x}
+  have hS : span_of_smul_tmul R A M N = Submodule.span A S := rfl
+  rw [Submodule.linearMap_eq_zero_iff_of_eq_span (S:=S) (hV:=hS)]
+  simp only [LinearMap.coe_comp, Submodule.coe_subtype, Function.comp_apply, Subtype.forall]
+  intro x hx
+  rw [Set.mem_setOf] at hx
+  obtain ⟨a, m, n, heq⟩ := hx
+  rw [← heq]
+  simp [mapOfCompatibleSMul', smul_tmul]
+
+abbrev quot_to_tens := Submodule.liftQ  (span_of_smul_tmul R A M N) (mapOfCompatibleSMul' A R M N)
+  (span_in_ker R A M N)
+
+@[simp]
+lemma quot_to_tens_apply (m : M) (n : N) : quot_to_tens R A M N ⟦m⊗ₜn⟧ = m⊗ₜn := by rfl
+
+
+def quot_equi_tens : (M ⊗[R] N ⧸ (span_of_smul_tmul R A M N)) ≃ₗ[A] M ⊗[A] N where
+  toLinearMap := quot_to_tens R A M N
+  invFun := tens_to_quot R A M N
+  left_inv := by
+    rw [Function.leftInverse_iff_comp]
+    ext z
+    simp only [AddHom.toFun_eq_coe, LinearMap.coe_toAddHom, Function.comp_apply, id_eq]
+    obtain ⟨y, hy⟩ := Quotient.exists_rep z
+    obtain ⟨S, h⟩ := exists_finset (y)
+    simp_rw [← hy, h]
+    clear h hy
+    simp_rw [Submodule.Quotient.mk''_eq_mk, ← Submodule.mkQ_apply, map_sum, Submodule.mkQ_apply,
+      ← Submodule.Quotient.mk''_eq_mk]
+    simp only [quot_to_tens_apply, tens_to_quot_apply]
+  right_inv := by
+    rw [Function.rightInverse_iff_comp]
+    ext z
+    simp only [AddHom.toFun_eq_coe, LinearMap.coe_toAddHom, Function.comp_apply, id_eq]
+    obtain ⟨S, h⟩ := exists_finset (z)
+    simp_rw [h]
+    simp
 
 lemma CompatibleSMul_ker_eq_span : (mapOfCompatibleSMul' A R M N).ker = (span_of_smul_tmul R A M N)
     := by
-  refine Eq.symm (Submodule.span_eq_of_le (mapOfCompatibleSMul' A R M N).ker ?_ ?_)
-  · sorry
-  · sorry
+  rw [← LinearEquiv.ker_comp (quot_equi_tens R A M N).symm (mapOfCompatibleSMul' A R M N)]
+  have h : (quot_equi_tens R A M N).symm.toLinearMap = tens_to_quot R A M N := rfl
+  rw [h, compose_eq_mkQ]
+  simp
